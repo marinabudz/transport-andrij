1547d890b9693cb5890de54ed0a6500ce1b8fdb0
marina budz <budz.marina97@gmail.com>
2020-08-04 00:08:00 +0300

    almost done
    missing header / footer

---
+++ data.js
@@ -0,0 +1,19 @@
+$(".slider-for").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: ".slider-nav"
+});
+$(".slider-nav").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: ".slider-for",
+  dots: false,
+  centerMode: true,
+  focusOnSelect: true,
+  infinite: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  arrows: false
+});
